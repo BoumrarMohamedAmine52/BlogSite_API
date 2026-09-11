2a3dbb4ee55f831f34f91643fe1b7b6ccc45fdb9
BoumrarMohamedAmine52 <boumrar.mohamedamine5252@gmail.com>
2026-09-11 16:55:14 +0200

    create the reaction Schema

---
+++ Models/reactionModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const reactionSchema = new mongoose.Schema(
+  {
+    isLike: {
+      type: Boolean,
+      required: [true, "a reaction must be either liking or disliking."],
+    },
+    from: {
+      type: mongoose.Schema.ObjectId,
+      ref: User,
+      required: [true, "a reaction must be by a user."],
+    },
+    targetType: {
+      type: String,
+      enum: {
+        values: ["post", "comment"],
+        message: "a target Type must be either a post or a comment.",
+      },
+      required: [true, "a reaction to target must have a type."],
+    },
+    targetId: {
+      // type : mongoose.Schema.ObjectId,
+      // ref : Post || Comment,
+      type: String,
+      required: [true, "a reaction must belong to either post or comment."],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Reaction = mongoose.Model("Reaction", reactionSchema);
+
+module.exports = Reaction;
