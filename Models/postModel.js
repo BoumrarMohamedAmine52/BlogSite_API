d6a73b1f00c4370c2ea131314a707211b2870187
BoumrarMohamedAmine52 <boumrar.mohamedamine5252@gmail.com>
2026-09-11 02:18:07 +0200

    create post Schema

---
+++ Models/postModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const User = require("./userModel");
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: User,
+      required: [true, "a post must belong to a user."],
+    },
+    title: {
+      type: String,
+      required: [true, "a post must have a title."],
+    },
+    content: {
+      type: String,
+      required: [true, "a post must have a content."],
+    },
+    coverImage: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Post = mongoose.Model("Post", postSchema);
+
+module.exports = Post;
