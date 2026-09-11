cf2e708586fb226710219d2b903d8ab3693356d8
BoumrarMohamedAmine52 <boumrar.mohamedamine5252@gmail.com>
2026-09-11 02:10:53 +0200

    create user schema

---
+++ Models/userModel.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "a user must have a username."],
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "a user must havean email."],
+      validate: [validator.isEmail, "please provide a valid email."],
+    },
+    password: {
+      type: String,
+      min: [8, "the password must be at least 8 caracters"],
+      required: [true, "please provide a password."],
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "please confirm ur password."],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+      },
+      select: false,
+    },
+    birthDate: {
+      type: Date,
+      required: [true, "please provide ur bith date."],
+    },
+    BirthPlace: {
+      type: String,
+      required: [true, "please provide ur birth place."],
+    },
+    bio: String,
+    gender: {
+      type: String,
+      required: [true, "please provide ur gender."],
+    },
+    ProfilePic: String,
+    isVerified: {
+      type: Boolean,
+      default: "false",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.Model("User", userSchema);
+
+module.exports = User;
