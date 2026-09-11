dfccb618a6b9575b9b0211a2327a6311e64e6818
BoumrarMohamedAmine52 <boumrar.mohamedamine5252@gmail.com>
2026-09-11 16:35:33 +0200

    create the repost schema

---
+++ Models/repostModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const User = require("./userModel");
+
+const repostSchema = new mongoose.Schema(
+  {
+    repostedBy: {
+      type: mongoose.Schema.ObjectId,
+      ref: User,
+      required: [true, "a reposted post must be by a user."],
+    },
+    originalPost: {
+      type: mongoose.Schema.ObjectId,
+      ref: Post,
+      required: [true, "please provide ur orginal post of the reposting."],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Repost = mongoose.Model("Repost", repostSchema);
+
+module.exports = Repost;
