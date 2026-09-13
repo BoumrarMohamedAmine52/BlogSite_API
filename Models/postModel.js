718e4606e53d302c0bc3e724647651808617c5ba
BoumrarMohamedAmine52 <boumrar.mohamedamine5252@gmail.com>
2026-09-13 18:01:57 +0200

    create post virtual properties

--- Models/postModel.js
+++ Models/postModel.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const User = require("./userModel");
+const Comment = require("./commentModel");
+const Reaction = require("./reactionModel");
+const Repost = require("./repostModel");
 
 const postSchema = new mongoose.Schema(
   {
@@ -22,6 +25,29 @@ const postSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+postSchema.virtual("commentsCount").get(async function () {
+  const comments = await Comment.find({ parentId: this.id });
+  return posts.length;
+});
+
+postSchema.virtual("likesCount").get(async function () {
+  const likes = await Reaction.find({ reactionTarget: this.id, isLike: true });
+  return likes.length;
+});
+
+postSchema.virtual("disLikesCount").get(async function () {
+  const dislikes = await Reaction.find({
+    reactionTarget: this.id,
+    isLike: false,
+  });
+  return dislikes.length;
+});
+
+postSchema.virtual("repostCount").get(async function () {
+  const reposts = await Repost.find({ originalPost: this.id });
+  return reposts.length;
+});
 
 const Post = mongoose.model("Post", postSchema);
 
